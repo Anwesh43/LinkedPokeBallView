6db6fbeb15b280169c37b7571eb5563e65cd807f
Anwesh43 <anweshthecool0@gmail.com>
2019-02-13 13:47:06 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedpokeballview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedpokeballview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedpokeballview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.pokeballview.PokeBallView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        PokeBallView.create(this)
     }
 }

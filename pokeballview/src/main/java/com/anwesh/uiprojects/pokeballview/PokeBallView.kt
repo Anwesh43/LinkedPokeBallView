d8b0991a72bf519a2869d1b8a2f45773942d1fa5
Anwesh43 <anweshthecool0@gmail.com>
2019-02-13 13:39:34 +0530

    adding PBNode class

--- pokeballview/src/main/java/com/anwesh/uiprojects/pokeballview/PokeBallView.kt
+++ pokeballview/src/main/java/com/anwesh/uiprojects/pokeballview/PokeBallView.kt
@@ -118,4 +118,48 @@ class PokeBallView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class PBNode(var i : Int, val state : State = State()) {
+
+        private var next : PBNode? = null
+        private var prev : PBNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = PBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawPokeBall(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : PBNode {
+            var curr : PBNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this 
+        }
+    }
 }
